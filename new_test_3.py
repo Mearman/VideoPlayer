c51f8e1c7d5766c3c1707fb96ed8c6ef618556d5
Joseph Mearman <joseph@mearman.co.uk>
2020-07-10 12:28:10 +0100

    refactor current frame

--- new_test_3.py
+++ new_test_3.py
@@ -50,14 +50,14 @@ controls_width = int(char_width * max([len(x) for x in command_text_array]))
 controls = numpy.zeros((controls_height, controls_width), numpy.uint8)
 
 y0, dy = line_height, line_height
-for i, line in enumerate(help_text.split('\n')):
-	y = y0 + i * dy
+for current_frame, line in enumerate(help_text.split('\n')):
+	y = y0 + current_frame * dy
 	cv2.putText(controls, line, (int(line_height / 2), y), cv2.FONT_HERSHEY_SIMPLEX, font_size, 255)
 
 cap = cv2.VideoCapture(video)
 tots = cap.get(cv2.CAP_PROP_FRAME_COUNT)
 
-i = 0
+current_frame = 0
 preIndex = 0
 frame_rate = 30
 bFirstInitUi = False
@@ -98,27 +98,27 @@ while True:
 	cv2.imshow(windows_controls, controls)
 	try:
 
-		if current_state != state_pause or preIndex != i:
-			if i == tots - 1:
-				i = 0
+		if current_state != state_pause or preIndex != current_frame:
+			if current_frame == tots - 1:
+				current_frame = 0
 
-			cv2.setTrackbarPos(frame_trackbar, window_video, i)
-			if preIndex != i - 1:
-				cap.set(cv2.CAP_PROP_POS_FRAMES, i)
+			cv2.setTrackbarPos(frame_trackbar, window_video, current_frame)
+			if preIndex != current_frame - 1:
+				cap.set(cv2.CAP_PROP_POS_FRAMES, current_frame)
 				print('call seek')
 
-			print('index', i)
+			print('index', current_frame)
 			ret, im = cap.read()
 			displayW = 1280.0
 			r = displayW / im.shape[1]
 			dim = (int(displayW), int(im.shape[0] * r))
 			im = cv2.resize(im, dim, interpolation=cv2.INTER_AREA)
 
-			cv2.putText(im, str(i), (10, 30), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 1)
+			cv2.putText(im, str(current_frame), (10, 30), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 1)
 			cv2.imshow(window_video, im)
 			if current_state != state_play:
 				current_state = state_pause
-			preIndex = i
+			preIndex = current_frame
 			if not bFirstInitUi:
 				bFirstInitUi = True
 
@@ -139,17 +139,16 @@ while True:
 			frame_rate = cv2.getTrackbarPos(speed_trackbar, window_video)
 			if frame_rate > 0:
 				sleep(1.0 / frame_rate)
-				i += 1
+				current_frame += 1
 			continue
 		if current_state == state_pause:
-			i = cv2.getTrackbarPos(frame_trackbar, window_video)
+			current_frame = cv2.getTrackbarPos(frame_trackbar, window_video)
 		if current_state == state_exit:
 			break
 		if current_state == state_skip_back:
-			i -= 1
-
+			current_frame -= 1
 		if current_state == state_skip_fwd:
-			i += 1
+			current_frame += 1
 		if current_state == state_speed_decrease:
 			frame_rate = max(frame_rate - fps_increment, 0)
 			cv2.setTrackbarPos(speed_trackbar, window_video, frame_rate)
@@ -158,13 +157,13 @@ while True:
 			cv2.setTrackbarPos(speed_trackbar, window_video, frame_rate)
 			current_state = state_play
 		if current_state == state_snapshot:
-			cv2.imwrite("./" + "Snap_" + str(i) + ".jpg", im)
-			print("Snap of Frame", {}, "Taken!", i)
+			cv2.imwrite("./" + "Snap_" + str(current_frame) + ".jpg", im)
+			print("Snap of Frame", {}, "Taken!", current_frame)
 		if current_state == state_skip_fwd_5:
-			i += 5
+			current_frame += 5
 		if current_state == state_skip_back_5:
-			i -= 5
-			i = i if (i >= 0) else 0
+			current_frame -= 5
+			current_frame = current_frame if (current_frame >= 0) else 0
 		if current_state == state_play_toggle:
 			current_state = state_pause if (preStatus == state_play) else state_play
 
