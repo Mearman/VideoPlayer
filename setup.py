9e53d8d79688d3674ebb190c24d87dd355fe03d0
Joseph Mearman <joseph@mearman.co.uk>
2020-07-22 09:05:35 +0100

    typo

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
 	name="ML-Fabeller",
 	version="1.0",
 	author="Joseph Mearman",
-	description="A tooled at labelleing video frames for ML training",
+	description="A tooled at labelling video frames for ML training",
 	url="https://github.com/Mearman/ML-Frabeller",
 	python_requires='>=3.6',
 	install_requires=["pandas>=1.0.5", "opencv-python>=4.3.0.36", "numpy>=1.19.1"]
