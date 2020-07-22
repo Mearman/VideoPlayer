9772a8c928e24713cd8bb8b58de3b8909a517ccc
Joseph Mearman <joseph@mearman.co.uk>
2020-07-22 09:04:34 +0100

    adds version numbers to setup.py

--- setup.py
+++ setup.py
@@ -10,5 +10,5 @@ setup(
 	description="A tooled at labelleing video frames for ML training",
 	url="https://github.com/Mearman/ML-Frabeller",
 	python_requires='>=3.6',
-	install_requires=["pandas", "opencv-python", "numpy"]
+	install_requires=["pandas>=1.0.5", "opencv-python>=4.3.0.36", "numpy>=1.19.1"]
 )
